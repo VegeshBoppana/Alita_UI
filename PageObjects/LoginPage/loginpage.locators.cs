edeb6861f00943921b14f4025a49c53d2ec80dcc
Vegesh Boppana <saivegeshbopp@gmail.com>
2024-10-15 16:15:25 +0530

    Add project files.

---
+++ PageObjects/LoginPage/loginpage.locators.cs
@@ -0,0 +1,17 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PageObjects.LoginPage
+{
+    public partial class loginpage
+    {
+        protected By UsernameFiled = By.CssSelector("#user-name");
+        protected By PasswordField = By.CssSelector("#password");
+        protected By LoginButton = By.CssSelector(".btn_action");
+
+    }
+}
