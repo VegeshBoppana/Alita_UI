56688d039d53130615f6be5c58281d01516decd6
Vegesh <87629978+VegeshBoppana@users.noreply.github.com>
2024-11-08 17:39:13 +0530

    Update UI_Alita/Utilities/SeleniumMethods.cs

--- UI_Alita/Utilities/SeleniumMethods.cs
+++ UI_Alita/Utilities/SeleniumMethods.cs
@@ -18,4 +18,38 @@ public class SeleniumMethods
     }
 
     // Add more methods as required for the framework
+
+    public void SwitchToWindow(string windowHandle)
+    {
+        Driver.SwitchTo().Window(windowHandle);
+    }
+
+    public void WaitForElementVisible(IWebElement element, int timeoutInSeconds)
+    {
+        WebDriverWait wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(timeoutInSeconds));
+        wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(element));
+    }
+
+    public void SelectDropDown(IWebElement element, string value)
+    {
+        SelectElement select = new SelectElement(element);
+        select.SelectByValue(value);
+    }
+
+    public void AcceptAlert()
+    {
+        Driver.SwitchTo().Alert().Accept();
+    }
+
+    public void TakeScreenshot(string filename)
+    {
+        Screenshot screenshot = ((ITakesScreenshot)Driver).GetScreenshot();
+        screenshot.SaveAsFile(filename, ScreenshotImageFormat.Png);
+    }
+
+    public void MoveToElementAndClick(IWebElement element)
+    {
+        Actions actions = new Actions(Driver);
+        actions.MoveToElement(element).Click().Perform();
+    }
 }
